4b368431e5c0743129d1cae0edff644570ae4181
GregTerekhov <113180004+GregTerekhov@users.noreply.github.com>
2023-04-26 06:43:12 +0300

    second time deploy

---
+++ src/components/App/App.jsx
@@ -0,0 +1,51 @@
+import { useState } from 'react';
+import { FeedbackOptions } from '../FeedbackOptions/FeedbackOptions';
+import { Statistics } from '../Statistics/Statistics';
+import { Section } from '../Section/Section';
+import { Notification } from '../Notification/Notification';
+import { Container } from './App.styled';
+
+export const App = () => {
+  const [feedback, setFeedback] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+
+  const handleFeedback = option => {
+    setFeedback(prevState => ({
+      ...prevState,
+      [option]: prevState[option] + 1,
+    }));
+  };
+
+  const totalFeedback = Object.values(feedback).reduce(
+    (total, currentValue) => total + currentValue
+  );
+
+  const positivePercentage = Math.round((feedback.good / totalFeedback) * 100);
+
+  return (
+    <Container>
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          onLeaveFeedback={handleFeedback}
+          options={Object.keys(feedback)}
+        />
+      </Section>
+      <Section title="Statistics">
+        {totalFeedback ? (
+          <Statistics
+            good={feedback.good}
+            neutral={feedback.neutral}
+            bad={feedback.bad}
+            total={totalFeedback}
+            positivePercentage={positivePercentage}
+          />
+        ) : (
+          <Notification message="There is no feedback" />
+        )}
+      </Section>
+    </Container>
+  );
+};
